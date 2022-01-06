b9560b28be0bfa2da6812585bb5c5386d2c12a05
ljdyer <74581995+ljdyer@users.noreply.github.com>
2022-01-06 19:50:16 +0000

    add missing comma between names check

--- site/js/checks.js
+++ site/js/checks.js
@@ -5,17 +5,29 @@ checks = {
         "short-desc": "Leading/trailing space",
         "help": "You may have some spaces at the start or end of lines.<br>Consider removing."
     },
+    "doubleSpace": {
+        "regex": RegExp(/  +/, "g"),
+        "class": "teal",
+        "short-desc": "Double space",
+        "help": "You may have some double spaces in your reference list.<br>Consider removing."
+    },
     "spaceBetweenInitials": {
         "regex": RegExp(/\w\.\s+\w\./, "g"),
         "class": "red",
         "short-desc": "Space between initials",
         "help": "Quick Start Guide available on University website has names with more than one initial with no spaces between initials<br>(e.g. Perkin, G.D.)."
     },
-    "doubleSpace": {
-        "regex": RegExp(/  +/, "g"),
-        "class": "teal",
-        "short-desc": "Double space",
-        "help": "You may have some double spaces in your reference list.<br>Consider removing."
+    "missingCommaAfterSurname": {
+        'regex': RegExp(/\b\w+\s([A-Z]\.)+/, "g"),
+        'class': 'yellow',
+        "short-desc": "Missing comma after surname",
+        "help": "A comma is required after each surname before the initials<br>(e.g. Watt, I.S.)"
+    },
+    'missingCommaBetweenNames': {
+        'regex': RegExp(/\w+, (\w\.)+ \w+, (\w\.)+/, "g"),
+        'class': 'beige',
+        'short-desc': 'Missing comma between names',
+        'help': "A comma is required between names in lists of 3 or more authors, expect before the 'and' (e.g. (e.g. Ratnawati, V., Freddy, D. and Hardi, H.)"
     },
     "missingAnd": {
         "regex": RegExp(/(?<!and) \S+,[^\n,]*\(\d{4}/, "g"),
@@ -23,11 +35,11 @@ checks = {
         "short-desc": "Missing 'and'",
         "help": "Quick Start Guide available on University website has 'and' before the last name in lists of names<br>(e.g. Ratnawati, V., Freddy, D. and Hardi, H.)"
     },
-    "missingCommaAfterSurname": {
-        'regex': RegExp(/\b\w+\s([A-Z]\.)+/, "g"),
-        'class': 'yellow',
-        "short-desc": "Missing comma after surname",
-        "help": "A comma is required after each surname before the initials<br>(e.g. Watt, I.S.)"
+    "serialComma": {
+        'regex': RegExp(/,\s?and/, "g"),
+        'class': 'navy',
+        'short-desc': 'Serial comma',
+        'help': "Quick Start Guide available on University website has lists of three or more names without serial commas<br>(e.g. Ratnawati, V., Freddy, D. and Hardi, H.)."
     },
     "doiInCaps": {
         'regex': RegExp(/DOI|Doi/, "g"),
@@ -41,17 +53,10 @@ checks = {
         "short-desc": "Full stop after doi",
         'help': "Quick Start Guide available on University website has no full stop at the end of lines ending with a DOI"
     },
-    "serialComma": {
-        'regex': RegExp(/,\s?and/, "g"),
-        'class': 'navy',
-        'short-desc': 'Serial comma',
-        'help': "Quick Start Guide available on University website has lists of three or more names without serial commas<br>(e.g. Ratnawati, V., Freddy, D. and Hardi, H.)."
-    },
     "etAl": {
         'regex': RegExp(/[Ee][Tt] [Aa][Ll]/, "g"),
         'class': 'maroon',
         'short-desc': 'Et al.',
         'help': "'Et al.' belongs in in-text citations. Authors should be listed in full in the list of references."
-    }
-    // TODO: Add check for missing comma between names
+    },
 }
